3c3714f0e26addaa2dea4a96f2e10d77a3c618e9
Masterflash <sxq@yandex.ru>
2017-10-15 17:56:57 +0300

    fix

--- src/View/Helper/Factory/Seolist.php
+++ src/View/Helper/Factory/Seolist.php
@@ -12,7 +12,7 @@ class Seolist implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-	   $cache = $container->get('FilesystemCache');
+	   $cache = $container->get('DefaultSystemCache');
 	   $connection=$container->get('ADO\Connection');
         return new $requestedName($connection,$cache);
     }
