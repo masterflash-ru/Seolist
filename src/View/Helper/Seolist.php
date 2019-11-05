b833e43bd52519384ad5be93a1b878f100bc622a
Masterflash <sxq@yandex.ru>
2019-11-05 08:25:03 +0300

    Update Seolist.php

--- src/View/Helper/Seolist.php
+++ src/View/Helper/Seolist.php
@@ -24,24 +24,23 @@ public function __invoke($sysname,$locale="ru_RU")
 	 $result = false;
 	 $key="Seolist_".preg_replace('/[^0-9a-zA-Z_\-]/iu', '',$sysname)."_{$locale}";
 	
-	$rez['title']="";
-	$rez['keywords']="";
-	$rez['description']="";
      $rez = $this->cache->getItem($key, $result);
-     if (!$result)
-        {
-			$rs=new RecordSet();
-			$rs->CursorType = adOpenKeyset;
-			$rs->open("select * from seolist where sysname='$sysname' and locale='$locale'",$this->connection);
-			if (!$rs->EOF)
-				{
-					$rez['title']=$rs->Fields->Item["title"]->Value;
-					$rez['keywords']=$rs->Fields->Item["keywords"]->Value;
-					$rez['description']=$rs->Fields->Item["description"]->Value;
-					$this->cache->setItem($key, $rez);
-					$this->cache->setTags($key,["Seolist"]);
-				}
-		}
+     if (!$result) {
+         $rs=new RecordSet();
+         $rs->CursorType = adOpenKeyset;
+         $rs->open("select * from seolist where sysname='$sysname' and locale='$locale'",$this->connection);
+         if (!$rs->EOF){
+             $rez['title']=$rs->Fields->Item["title"]->Value;
+             $rez['keywords']=$rs->Fields->Item["keywords"]->Value;
+             $rez['description']=$rs->Fields->Item["description"]->Value;
+             $this->cache->setItem($key, $rez);
+             $this->cache->setTags($key,["Seolist"]);
+         } else {
+             $rez['title']="";
+             $rez['keywords']="";
+             $rez['description']="";
+         }
+     }
 	$view=$this->getView();
 	$view->headTitle($rez['title']);
 	$view->headMeta()->appendName('keywords', $rez['keywords']);
