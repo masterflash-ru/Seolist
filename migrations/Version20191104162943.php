e6e03369800443788afc1f2b0744435b6d81e9a7
Masterflash <sxq@yandex.ru>
2019-11-27 15:16:02 +0300

    Update Version20191104162943.php

--- migrations/Version20191104162943.php
+++ migrations/Version20191104162943.php
@@ -4,46 +4,39 @@ namespace Mf\Seolist;
 
 use Mf\Migrations\AbstractMigration;
 use Mf\Migrations\MigrationInterface;
+use Zend\Db\Sql\Ddl;
 
 class Version20191104162943 extends AbstractMigration implements MigrationInterface
 {
-    public static $description = "Migration description";
+    public static $description = "SEO list module";
 
-    public function up($schema)
+    public function up($schema, $adapter)
     {
-        switch ($this->db_type){
-            case "mysql":{
-                $this->addSql("CREATE TABLE `seolist` (
-                  `id` int(11) NOT NULL AUTO_INCREMENT,
-                  `locale` char(20) NOT NULL COMMENT 'Локаль',
-                  `sysname` char(50) DEFAULT NULL COMMENT 'системное имя',
-                  `name` varchar(127) NOT NULL DEFAULT '' COMMENT 'имя понятное нам',
-                  `title` text NOT NULL,
-                  `keywords` text NOT NULL,
-                  `description` text NOT NULL,
-                  PRIMARY KEY (`id`),
-                  KEY `locale` (`locale`),
-                  KEY `sysname` (`sysname`)
-                ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Заголовки и ключевые слова'");
-                break;
-            }
-            default:{
-                throw new \Exception("the database {$this->db_type} is not supported !");
-            }
-        }
+        $this->mysql_add_create_table=" ENGINE=MyIsam DEFAULT CHARSET=utf8";
+        $table = new Ddl\CreateTable("seolist");
+        $table->addColumn(new Ddl\Column\Integer('id',false,null,["AUTO_INCREMENT"=>true]));
+        $table->addColumn(new Ddl\Column\Char('locale', 20,false,null,["COMMENT"=>"Локаль"]));
+        $table->addColumn(new Ddl\Column\Char('sysname', 50,false,null,["COMMENT"=>"Системное имя элемента"]));
+        $table->addColumn(new Ddl\Column\Char('name', 50,false,null,["COMMENT"=>"Имя элемента в админке"]));
+        $table->addColumn(new Ddl\Column\Char('label', 255,false,null,["COMMENT"=>"Текст элемента"]));
+        $table->addColumn(new Ddl\Column\Char('title', 255,false));
+        $table->addColumn(new Ddl\Column\Char('keywords', 255,false));
+        $table->addColumn(new Ddl\Column\Text('description', null,false));
+
+        $table->addConstraint(
+            new Ddl\Constraint\PrimaryKey(['id'])
+        );
+        $table->addConstraint(
+            new Ddl\Index\Index(['locale'],'locale')
+        )->addConstraint(
+            new Ddl\Index\Index(['sysname'],'sysname')
+        );
+        $this->addSql($table);
     }
 
-    public function down($schema)
+    public function down($schema, $adapter)
     {
-        //throw new \RuntimeException('No way to go down!');
-        switch ($this->db_type){
-            case "mysql":{
-                $this->addSql("CDROP TABLE `seolist`");
-                break;
-            }
-            default:{
-                throw new \Exception("the database {$this->db_type} is not supported !");
-            }
-        }
+        $drop = new Ddl\DropTable('seolist');
+        $this->addSql($drop);
     }
 }
