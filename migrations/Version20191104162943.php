27c1f15eb8be109e5372a9560e5dca75887c8d85
Masterflash <sxq@yandex.ru>
2019-12-06 11:11:27 +0300

    Update Version20191104162943.php

--- migrations/Version20191104162943.php
+++ migrations/Version20191104162943.php
@@ -18,7 +18,6 @@ class Version20191104162943 extends AbstractMigration implements MigrationInterf
         $table->addColumn(new Ddl\Column\Char('locale', 20,false,null,["COMMENT"=>"Локаль"]));
         $table->addColumn(new Ddl\Column\Char('sysname', 50,false,null,["COMMENT"=>"Системное имя элемента"]));
         $table->addColumn(new Ddl\Column\Char('name', 50,false,null,["COMMENT"=>"Имя элемента в админке"]));
-        $table->addColumn(new Ddl\Column\Char('label', 255,false,null,["COMMENT"=>"Текст элемента"]));
         $table->addColumn(new Ddl\Column\Char('title', 255,false));
         $table->addColumn(new Ddl\Column\Char('keywords', 255,false));
         $table->addColumn(new Ddl\Column\Text('description', null,false));
